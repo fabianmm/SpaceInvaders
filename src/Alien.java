627f9707c8637d88aed50aad57520fcd8ce1feee
Fabian <f.montemayor26@gmail.com>
2015-03-04 11:56:19 -0600

    Alien ya quedó con estándar

--- src/Alien.java
+++ src/Alien.java
@@ -1,58 +1,109 @@
 
 import javax.swing.ImageIcon;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
- * @author http://zetcode.com/
- */
+  * Alien
+  *
+  * Modela la definición de todos los objetos de tipo
+  * <code>Alien</code>
+  *
+  * @author Fabián Montemayor A01280156 & Mauro Amarante A01191903
+  * @version 2.0 
+  * @date 4/03/15
+  */
+
 public class Alien extends Sprite {
 
-    private Bomb bomb;
-    private final String shot = "alien.png";
+    private Bomb bmbBomb; // bomba del alien, objeto del tipo Bomb
+    private final String sShot = "alien.png";   // string con el url de la imagen
 
-    public Alien(int x, int y) {
-        this.iX = x;
-        this.iY = y;
-
-        bomb = new Bomb(x, y);
-        ImageIcon imiImagen = new ImageIcon(this.getClass().getResource(shot));
+    /**
+      * Alien
+      * 
+      * Metodo constructor usado para crear el objeto Alien.
+      * 
+      * @param iX es la variable para actualizar la posición x del alien
+      * @param iY es la variable para actualizar la posicion y del alien
+      */
+    public Alien(int iX, int iY) {
+        this.iX = iX;
+        this.iY = iY;
+        bmbBomb = new Bomb(iX, iY);
+        ImageIcon imiImagen = new ImageIcon(this.getClass().getResource(sShot));
         setImage(imiImagen.getImage());
-
     }
 
-    public void act(int direction) {
-        this.iX += direction;
+    /**
+     * act
+     * 
+     * Metodo de actualización de posición del alien.
+     * 
+     */
+    public void act(int iDirection) {
+        this.iX += iDirection;
     }
 
+    /**
+     * getBomb
+     * 
+     * Metodo de acceso que regresa el objeto Bomb del Alien.
+     * 
+     */
     public Bomb getBomb() {
-        return bomb;
+        return bmbBomb;
     }
 
+    /**
+      * Bomb
+      *
+      * Modela la definición de todos los objetos de tipo
+      * <code>Bomb</code>
+      *
+      * @author Fabián Montemayor A01280156 & Mauro Amarante A01191903
+      * 
+      */
     public class Bomb extends Sprite {
 
-        private final String bomb = "bomb.png";
-        private boolean destroyed;
+        private final String sBomb = "bomb.png";    // string del url de la imagen
+        private boolean bDestroyed; // boleana de si la bomba está destruida o no
 
-        public Bomb(int x, int y) {
+        /**
+          * Bomb
+          * 
+          * Metodo constructor del objeto Bomb.
+          * 
+          * @param iX es la posición x de la bomba.
+          * @param iY es la posición y de la bomba.
+          * 
+          */
+        public Bomb(int iX, int iY) {
             setDestroyed(true);
-            this.iX = x;
-            this.iY = y;
-            ImageIcon ii = new ImageIcon(this.getClass().getResource(bomb));
-            setImage(ii.getImage());
+            this.iX = iX;
+            this.iY = iY;
+            ImageIcon imiImagen = new ImageIcon(this.getClass().getResource(sBomb));
+            setImage(imiImagen.getImage());
         }
 
-        public void setDestroyed(boolean destroyed) {
-            this.destroyed = destroyed;
+        /**
+          * setDestroyed
+          * 
+          * Metodo de actualización que cambia el estado de la boleana bDestroyed.
+          * 
+          * @param bDestroyed es la boleana que va a actualizar.
+          * 
+          */
+        public void setDestroyed(boolean bDestroyed) {
+            this.bDestroyed = bDestroyed;
         }
-
+        
+        /**
+          * isDestroyed
+          * 
+          * Metodo de acceso que regresa el estado de la boleana bDestroyed de Bomb.
+          * 
+          */
         public boolean isDestroyed() {
-            return destroyed;
+            return bDestroyed;
         }
     }
 }
