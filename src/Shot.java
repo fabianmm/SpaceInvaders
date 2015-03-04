9d545ad00ab1649df8c786d161bc2c76e36457a6
Fabian <f.montemayor26@gmail.com>
2015-03-04 11:46:13 -0600

    Shot ya quedó con estándar

--- src/Shot.java
+++ src/Shot.java
@@ -1,30 +1,46 @@
 
 import javax.swing.ImageIcon;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Shot
+ *
+ * Modela la definición de todos los objetos de tipo
+ * <code>Shot</code>
+ *
+ * @author Fabián Montemayor A01280156 & Mauro Amarante A01191903
+ * @version 2.0 
+ * @date 4/03/15
  */
 
-/**
- *
- * @author http://zetcode.com/
- */
 public class Shot extends Sprite {
 
-    private String sShot = "shot.png";
-    private final int H_SPACE = 6;
-    private final int V_SPACE = 1;
+    private String sShot = "shot.png";  // string con el nombre de la imagen
+    private final int iH_SPACE = 6;     // constante H_SPACE 
+    private final int iV_SPACE = 1;     // constante V_SPACE
 
+    /**
+      * Shot
+      * 
+      * Metodo constructor usado para crear el objeto Shot cuando no contiene 
+      * parámetros.
+      * 
+      */
     public Shot() {
+        // no contiene código
     }
 
-    public Shot(int x, int y) {
-
+    /**
+      * Shot
+      * 
+      * Metodo constructor usado para crear el objeto Shot.
+      * 
+      * @param iX es la variable para actualizar la posición x del shot
+      * @param iY es la variable para actualizar la posicion y del shot
+      */
+    public Shot(int iX, int iY) {
         ImageIcon imiImagen = new ImageIcon(this.getClass().getResource(sShot));
-        setImage(imiImagen.getImage());
-        setX(x + H_SPACE);
-        setY(y - V_SPACE);
+        setImage(imiImagen.getImage()); // actualiza la imagen del shot
+        setX(iX + iH_SPACE);    // actualiza la x del shot
+        setY(iY - iV_SPACE);    // actualiza la y del shot
     }
 }
